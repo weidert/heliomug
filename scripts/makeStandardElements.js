dbd7dbc09697be06d7511b0389c0041ae0a64445
Craig Weidert <craig.weidert@gmail.com>
2017-01-11 22:34:12 -0800

    tried inlining other file, no speed up

--- scripts/makeStandardElements.js
+++ scripts/makeStandardElements.js
@@ -84,6 +84,28 @@ function makeBackToTopButton() {
   });
 }
 
+function getTopNavList() {
+  var $navList = $("<ul>");
+
+  $navList.append('<li><a href="http://heliomug.com">Home</a></li>');
+  $navList.append('<li><a href="http://heliomug.com/prog/java">Programs</a></li>');
+  $navList.append('<li><a href="http://heliomug.com/prog/visualizations">Data Vis</a></li>');
+  $navList.append('<li><a href="http://heliomug.com/prog/ml">Machine Learning</a></li>');
+  $navList.append('<li><a href="http://heliomug.com/caller/caller_description.html">Caller</a></li>');
+  $navList.append('<li><a href="http://heliomug.com/prog/web/fckc/">FCKC</a></li>');
+  $navList.append('<li><a href="http://heliomug.com/about.html">About</a></li>');
+
+  return $navList;
+}
+
+function getFooterContent() {
+  var $footerContent = $("<p>");
+  
+  $footerContent.append("<p>This page belongs to <a href='http://heliomug.com/about.html'>Craig Weidert</a></p>");
+  
+  return $footerContent;
+}
+
 function makeStandardStuff() {
   // makes the section nav list
   // if a nav#sectionNav exists
@@ -92,6 +114,7 @@ function makeStandardStuff() {
   // this function calls 
   makeBackToTopButton();
 
+  
   // imports a couple of functions to customize the stuff on your website
   $.getScript("/scripts/makeMyStuff.js").done(function() {
     makeSiteNav();
