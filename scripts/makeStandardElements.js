2fae211576498980d40e4f4d360a257371cff60d
Craig Weidert <craig.weidert@gmail.com>
2017-01-17 22:58:02 -0800

    removed references to heliomug.com

--- scripts/makeStandardElements.js
+++ scripts/makeStandardElements.js
@@ -86,28 +86,6 @@ function makeBackToTopButton() {
   });
 }
 
-function getTopNavList() {
-  var $navList = $("<ul>");
-
-  $navList.append('<li><a href="http://heliomug.com">Home</a></li>');
-  $navList.append('<li><a href="http://heliomug.com/prog/java">Programs</a></li>');
-  $navList.append('<li><a href="http://heliomug.com/prog/visualizations">Data Vis</a></li>');
-  $navList.append('<li><a href="http://heliomug.com/prog/ml">Machine Learning</a></li>');
-  $navList.append('<li><a href="http://heliomug.com/caller/caller_description.html">Caller</a></li>');
-  $navList.append('<li><a href="http://heliomug.com/prog/web/fckc/">FCKC</a></li>');
-  $navList.append('<li><a href="http://heliomug.com/about.html">About</a></li>');
-
-  return $navList;
-}
-
-function getFooterContent() {
-  var $footerContent = $("<p>");
-  
-  $footerContent.append("<p>This page belongs to <a href='http://heliomug.com/about.html'>Craig Weidert</a></p>");
-  
-  return $footerContent;
-}
-
 function makeStandardStuff() {
   // makes the section nav list
   // if a nav#sectionNav exists
