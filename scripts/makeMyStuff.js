2fba8c0022710bea7351ddd57a599ae11c05045a
Craig Weidert <craig.weidert@gmail.com>
2017-01-18 22:12:14 -0800

    added a bunch of facebook open graph stuff, etc

--- scripts/makeMyStuff.js
+++ scripts/makeMyStuff.js
@@ -12,7 +12,6 @@ function getTopNavList() {
   $navList.append('<li><a href="' + basename + '">Home</a></li>');
   $navList.append('<li><a href="' + basename + 'prog/java">Programs</a></li>');
   $navList.append('<li><a href="' + basename + 'prog/visualizations">Data Vis</a></li>');
-  $navList.append('<li><a href="' + basename + 'prog/ml">Machine Learning</a></li>');
   $navList.append('<li><a href="' + basename + 'caller/caller_description.html">Caller</a></li>');
   $navList.append('<li><a href="' + basename + 'prog/web/fckc/">FCKC</a></li>');
   $navList.append('<li><a href="' + basename + 'about.html">About</a></li>');
