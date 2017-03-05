c11f1c4ab1910c03e182b587782db3fdbf13162f
Craig Weidert <craig.weidert@gmail.com>
2017-03-05 13:00:37 -0800

    moved google analytics script to makeStandard...

--- scripts/makeStandardElements.js
+++ scripts/makeStandardElements.js
@@ -96,7 +96,7 @@ function makeStandardStuff() {
 
   // imports a couple of functions to customize the stuff on your website
   $.getScript("/scripts/makeMyStuff.js").done(function() {
-    makeOtherStuff();
+    // makeOtherStuff();
     makeSiteNav();
     makeFooter();
   }).fail(function( jqxhr, settings, exception ) {
@@ -110,3 +110,19 @@ function makeStandardStuff() {
   handleWindowWidth();
   window.onresize = handleWindowWidth;
 }
+
+(function(i,s,o,g,r,a,m) { 
+    i['GoogleAnalyticsObject']=r;
+    i[r]=i[r] || function() {
+        (i[r].q=i[r].q||[]).push(arguments)
+    }, i[r].l=1*new Date();
+    a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];
+    a.async=1;
+    a.src=g;
+    m.parentNode.insertBefore(a,m)
+})
+(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-92960895-1', 'auto');
+ga('send', 'pageview');
+
